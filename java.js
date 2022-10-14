14048d6d79ae394caede7b8afeb859abc588ccdb
BirdyMaster <domassirbike@gmail.com>
2022-10-14 21:11:53 +0200

    Fix double click bugs. Change CSS -------------------------------------
    -------------------------------------

--- java.js
+++ java.js
@@ -8,32 +8,43 @@ let start = 0;
 let stop;
 let timePassed = 0
 let timerDisplay;
+let timeRunning = false
 
 startBtn.addEventListener('click', () => {
-
-    if (!timePassed == 0) {start = new Date().getTime() - (- timePassed)}
-    // if the clock has been stopped before, use this value as starting point
-
-        else {start = new Date().getTime()} 
-        //if not, the starting point is NOW
     
-    stop = undefined
-    //reset the stop-point, which is redefined on every stop
-    // if this is defined, nothing funny happens on second click
+    if (timeRunning === false) {
 
-    timerDisplay = setInterval(() => {
-        let timeGap = new Date().getTime() - start
-        timeText.textContent = (timeGap / 1000).toFixed(2)
-    }, 50)
+        if (!timePassed == 0) {
+            start = new Date().getTime() - (- timePassed)
+            // if the clock has been stopped before, use this value as starting point
+        } else if (timePassed == 0) {
+            start = new Date().getTime()} 
+            //if not, the starting point is NOW
+
+        timeRunning = true
+        stop = undefined
+        //reset the stop-point, which is redefined on every stop
+        // if this is defined, nothing funny happens on second click
+
+        timerDisplay = setInterval(() => {
+            let timeGap = new Date().getTime() - start
+            timeText.textContent = (timeGap / 1000).toFixed(2)
+        }, 50)
+
+    }
 })
 
 
 stopBtn.addEventListener('click', () => {
-    if (!stop) {
+   
+    if (!stop && timeRunning === true) {
+
         stop = new Date().getTime()
         timePassed = start - stop
+        timeRunning = false
+        clearInterval(timerDisplay, 50)
     } 
-    clearInterval(timerDisplay, 50)
+    
 })
 
 
@@ -41,7 +52,8 @@ resetBtn.addEventListener('click', () => {
     stop = undefined
     start = undefined
     timePassed = 0
-    timeText.textContent = 0
+    timeText.textContent = "0.00"
+    timeRunning = false
     clearInterval(timerDisplay, 50)
 })
 
