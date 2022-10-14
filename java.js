885eb3e03a733d4d0bbe35535735e3e31a8922da
BirdyMaster <domassirbike@gmail.com>
2022-10-14 21:47:51 +0200

    Add UX features. Slightly change CSS -------------------------------------
    -------------------------------------

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ const startBtn = document.querySelector('.startBtn')
 const stopBtn = document.querySelector('.stopBtn')
 const resetBtn = document.querySelector('.resetBtn')
 const timeText = document.querySelector('.timeText')
-
+const warningText = document.querySelector('.warning')
 
 let start = 0;
 let stop;
@@ -14,12 +14,13 @@ startBtn.addEventListener('click', () => {
     
     if (timeRunning === false) {
 
-        if (!timePassed == 0) {
-            start = new Date().getTime() - (- timePassed)
+        if (timePassed == 0) {
+            start = new Date().getTime()
+            //if this is the first click on the start button, the starting point is NOW
             // if the clock has been stopped before, use this value as starting point
-        } else if (timePassed == 0) {
-            start = new Date().getTime()} 
-            //if not, the starting point is NOW
+        } else if (!timePassed == 0) {
+            start = new Date().getTime() - (- timePassed)} 
+            //if not, create a new date and subtract it with the already runned time
 
         timeRunning = true
         stop = undefined
@@ -31,6 +32,17 @@ startBtn.addEventListener('click', () => {
             timeText.textContent = (timeGap / 1000).toFixed(2)
         }, 50)
 
+        timeText.classList.remove('yellowBack')
+        timeText.classList.add('greenBack')
+        warningText.style.opacity = '0%'
+
+    } else if (!timeRunning === false) {
+
+        warningText.style.opacity = '100%'
+        warningText.textContent = "Time already running"
+        setTimeout(() => {
+            warningText.style.opacity = '0%'
+        }, 1500);
     }
 })
 
@@ -43,17 +55,47 @@ stopBtn.addEventListener('click', () => {
         timePassed = start - stop
         timeRunning = false
         clearInterval(timerDisplay, 50)
-    } 
+
+        timeText.classList.add('yellowBack')
+        timeText.classList.remove('greenBack')
+        warningText.style.opacity = '0%'
+
+    } else if (!timeRunning === true) {
+
+        warningText.style.opacity = '100%'
+        warningText.textContent = "Time already stopped"
+        setTimeout(() => {
+            warningText.style.opacity = '0%'
+        }, 1500);
+    }
     
 })
 
 
 resetBtn.addEventListener('click', () => {
-    stop = undefined
-    start = undefined
-    timePassed = 0
-    timeText.textContent = "0.00"
-    timeRunning = false
-    clearInterval(timerDisplay, 50)
+
+    if (timeRunning == false && (!stop) ) {
+        console.log('work');
+        warningText.style.opacity = '100%'
+        warningText.textContent = "Time already stopped"
+        setTimeout(() => {
+            warningText.style.opacity = '0%'
+        }, 1500);
+    } else {
+        stop = undefined
+        start = undefined
+        timePassed = 0
+        timeText.textContent = "0.00"
+        timeRunning = false
+        clearInterval(timerDisplay, 50)
+
+        timeText.classList.remove('yellowBack', 'greenBack')
+        warningText.style.opacity = '0%'
+    }
+    
+
+    
+
+    
 })
 
